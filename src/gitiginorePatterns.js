4406658956c08d68f3a528826947f37cae3d4287
Sy Le <lenguyensy@gmail.com>
2022-02-27 11:25:29 -0800

    Renamed `file.js` to `fileUtils.js` and extracted writeFile to `fileUtils` (#23)
    * minor update to the text
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- src/gitiginorePatterns.js
+++ src/gitiginorePatterns.js
@@ -1,16 +1,18 @@
-const file = require("./file");
+const fileUtils = require("./fileUtils");
 const path = require("path");
 
 // figuring out what files in gitignore to skip
 let gitiginorePatterns = [];
 
 try {
-  const gitignoreContent = file.read(path.join(process.cwd(), ".gitignore"));
+  const gitignoreContent = fileUtils.read(
+    path.join(process.cwd(), ".gitignore")
+  );
   gitiginorePatterns = gitignoreContent
     .split("\n")
     .filter((s) => !s.includes("#") && !s.includes("*") && s);
 } catch (err) {
-  console.log("failed to read git ignore", err);
+  console.log("[Warning] Failed to read .gitignore".yellow);
 }
 
 module.exports = gitiginorePatterns;
