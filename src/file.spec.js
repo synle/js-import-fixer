d7656d0fe26a6a18f5059c92344dbccf10c29ea6
synle <synle@users.noreply.github.com>
2022-02-27 15:08:35 +0000

    CI / CD - Prettier Automatic Commit

--- src/file.spec.js
+++ src/file.spec.js
@@ -1,24 +1,24 @@
-const path = require('path');
-const file = require('./file');
+const path = require("path");
+const file = require("./file");
 
-describe('file.js', () => {
-  test('read', async () => {
-      const actual = file.read(path.join(process.cwd(), 'package.json'));
-      expect(actual).toBeDefined();
-      expect(actual).toContain(`"version"`);
-    });
+describe("file.js", () => {
+  test("read", async () => {
+    const actual = file.read(path.join(process.cwd(), "package.json"));
+    expect(actual).toBeDefined();
+    expect(actual).toContain(`"version"`);
+  });
 
-    test('readJson', async () => {
-      const actual = file.readJson(path.join(process.cwd(), 'package.json'));
-      expect(actual.name).toBeDefined();
-      expect(actual.description).toBeDefined();
-      expect(actual.version).toBeDefined();
-    });
+  test("readJson", async () => {
+    const actual = file.readJson(path.join(process.cwd(), "package.json"));
+    expect(actual.name).toBeDefined();
+    expect(actual.description).toBeDefined();
+    expect(actual.version).toBeDefined();
+  });
 
-    test('listDirNested', async () => {
-      const actual = file.listDirNested(process.cwd());
-      expect(actual.length > 0).toBe(true);
-      expect(actual.some(f => f.includes("/src/index.js"))).toBe(true)
-      expect(actual.some(f => f.includes("/src/coreUtils.js"))).toBe(true)
-    });
-  })
+  test("listDirNested", async () => {
+    const actual = file.listDirNested(process.cwd());
+    expect(actual.length > 0).toBe(true);
+    expect(actual.some((f) => f.includes("/src/index.js"))).toBe(true);
+    expect(actual.some((f) => f.includes("/src/coreUtils.js"))).toBe(true);
+  });
+});
