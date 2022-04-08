399afff68392153757a3f48a91f5bc71b8ea405d
Sy Le <lenguyensy@gmail.com>
2022-04-07 21:02:08 -0700

    wip added tests

---
+++ __mocks__/nested_dir_a/nested_dir_b/sample_3.js
@@ -0,0 +1,6 @@
+import {sum, diff} from '../Calculator';
+import externalLib1 from './externalLib1';
+import ServerApi from './ServerApi';
+import {methodLib2, constant2} from "externalLib2";
+
+const res1 = sum(1,2);
