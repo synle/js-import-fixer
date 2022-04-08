38c5b6b87a9e1d6133671245bde7700c58eb6330
Sy Le <lenguyensy@gmail.com>
2022-04-08 10:25:02 -0700

    Fixed up the build number `${year}${month}${day}${hour}${minute}` (#41)
    * clean up build numbers
    
    * 0.4.0
    
    * CI / CD - Prettier Automatic Commit
    
    Co-authored-by: synle <synle@users.noreply.github.com>

--- .github/publishMain.js
+++ .github/publishMain.js
@@ -2,10 +2,20 @@ const fs = require("fs");
 const oldPackageJson = JSON.parse(fs.readFileSync("./package.json"));
 const oldVersion = oldPackageJson.version;
 
-// construct the new temp build
+// get the number from package version
 const versionSplits = oldPackageJson.version.split(/[\.-]/);
 const [major, minor, patch, postfix] = versionSplits;
-const newTempVersion = `${major}.${minor}.${patch}-${Date.now()}`;
+
+// get the number from now timestamps
+const year = new Date().getFullYear().toString().substr(2);
+const month = (new Date().getMonth() + 1).toString().padStart(2, "0");
+const day = new Date().getDate().toString().padStart(2, "0");
+const hour = new Date().getHours().toString().padStart(2, "0");
+const minute = new Date().getMinutes().toString().padStart(2, "0");
+const fullVersionName = `${year}${month}${day}${hour}${minute}`;
+
+// construct the new temp build
+const newTempVersion = `${major}.${minor}.${fullVersionName}`;
 
 // set the new temp version
 oldPackageJson.version = newTempVersion;
