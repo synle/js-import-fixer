0c832334febb3f7f2ea937af30b106c97921afe0
synle <synle@users.noreply.github.com>
2022-04-08 17:23:43 +0000

    CI / CD - Prettier Automatic Commit

--- .github/publishMain.js
+++ .github/publishMain.js
@@ -7,12 +7,12 @@ const versionSplits = oldPackageJson.version.split(/[\.-]/);
 const [major, minor, patch, postfix] = versionSplits;
 
 // get the number from now timestamps
-const year = (new Date().getFullYear()).toString().substr(2);
-const month = (new Date().getMonth() + 1).toString().padStart(2, '0');
-const day = new Date().getDate().toString().padStart(2, '0');
-const hour = new Date().getHours().toString().padStart(2, '0');
-const minute = new Date().getMinutes().toString().padStart(2, '0');
-const fullVersionName = `${year}${month}${day}${hour}${minute}`
+const year = new Date().getFullYear().toString().substr(2);
+const month = (new Date().getMonth() + 1).toString().padStart(2, "0");
+const day = new Date().getDate().toString().padStart(2, "0");
+const hour = new Date().getHours().toString().padStart(2, "0");
+const minute = new Date().getMinutes().toString().padStart(2, "0");
+const fullVersionName = `${year}${month}${day}${hour}${minute}`;
 
 // construct the new temp build
 const newTempVersion = `${major}.${minor}.${fullVersionName}`;
