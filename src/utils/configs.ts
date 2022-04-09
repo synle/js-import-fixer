6d869570f15b3f07be36e379a6e6b098fb806a65
Sy Le <lenguyensy@gmail.com>
2022-04-08 22:13:53 -0700

    refactor and test configs

--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -1,4 +1,4 @@
-type Config =  {
+type Config = {
   isTest: boolean;
   groupImport: boolean;
   filteredFiles: string[];
@@ -8,41 +8,43 @@ type Config =  {
   importQuote: string;
 };
 
-const configs: Config = {
-  isTest: !!process.env.JEST_WORKER_ID,
-  groupImport: false,
-  filteredFiles: [],
-  ignoredFiles: [],
-  aggressiveCheck: false,
-  transformRelativeImport: undefined,
-  importQuote: `'`,
-};
+export function getConfigs(argvs: string[]) {
+  const configs: Config = {
+    isTest: !!process.env.JEST_WORKER_ID,
+    groupImport: false,
+    filteredFiles: [],
+    ignoredFiles: [],
+    aggressiveCheck: false,
+    transformRelativeImport: undefined,
+    importQuote: `'`,
+  };
 
-for (const argv of process.argv) {
-  if (argv.includes(`--groupImport`)) {
-    configs.groupImport = true;
+  for (const argv of argvs) {
+    if (argv.includes(`--groupImport`)) {
+      configs.groupImport = true;
+    }
+    if (argv.includes(`--filter=`)) {
+      configs.filteredFiles = argv.substr(argv.indexOf(`=`) + 1).split(`,`);
+    }
+    if (argv.includes(`--ignored=`)) {
+      configs.ignoredFiles = argv.substr(argv.indexOf(`=`) + 1).split(`,`);
+    }
+    if (argv.includes(`--aggressive`)) {
+      configs.aggressiveCheck = true;
+    }
+    if (argv.includes(`--transformRelativeImport=`)) {
+      configs.transformRelativeImport = argv.substr(argv.indexOf(`=`) + 1).replace(/"/g, '');
+    } else if (argv.includes(`--transformRelativeImport`)) {
+      configs.transformRelativeImport = '';
+    }
+    if (argv.includes(`--importQuote=`)) {
+      configs.importQuote = argv.substr(argv.indexOf(`=`) + 1).trim() !== 'single' ? `"` : `'`;
+    }
   }
-  if (argv.includes(`--filter=`)) {
-    configs.filteredFiles = argv.substr(argv.indexOf(`=`) + 1).split(`,`);
-  }
-  if (argv.includes(`--ignored=`)) {
-    configs.ignoredFiles = argv.substr(argv.indexOf(`=`) + 1).split(`,`);
-  }
-  if (argv.includes(`--aggressive`)) {
-    configs.aggressiveCheck = true;
-  }
-  if (argv.includes(`--transformRelativeImport=`)) {
-    configs.transformRelativeImport = argv
-      .substr(argv.indexOf(`=`) + 1)
-      .replace(/"/g, "");
-  } else if (argv.includes(`--transformRelativeImport`)) {
-    configs.transformRelativeImport = "";
-  }
-  if (argv.includes(`--importQuote=`)) {
-    configs.importQuote =
-      argv.substr(argv.indexOf(`=`) + 1).trim() !== "single" ? `"` : `'`;
-  }
+  return configs;
 }
+
+const configs = getConfigs(process.argv);
 
 // hooking up logging
 if (configs.isTest) {
