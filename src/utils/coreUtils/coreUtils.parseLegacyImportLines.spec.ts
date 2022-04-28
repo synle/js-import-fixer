6ec734294f2f2a9f00e70452edc95f6e1b6480ed
Sy Le <lenguyensy@gmail.com>
2022-04-27 18:08:28 -0700

    the regex for full const require...

--- src/utils/coreUtils/coreUtils.parseLegacyImportLines.spec.ts
+++ src/utils/coreUtils/coreUtils.parseLegacyImportLines.spec.ts
@@ -2,6 +2,18 @@
 import configs from 'src/utils/configs';
 import coreUtils from 'src/utils/coreUtils';
 
+/**
+var http = require('http');
+let fs = require('fs');
+const { host, port } = require('./config');
+var path = require("path");
+let child_process = require("child_process");
+const apiClient = require("./utils/api/apiClient");
+const database=require('database')
+const network = require(     'network'     )
+var moment = require(    "moment")
+var faker = require("faker"       )
+ */
 describe('coreUtils.parseLegacyImportLines', () => {
   const fileNameSample1 = '/git/api-server/Server.ts';
   const importSample1 = [
@@ -11,14 +23,16 @@ describe('coreUtils.parseLegacyImportLines', () => {
     `var path = require("path");`,
     `let child_process = require("child_process");`,
     `const apiClient = require("./utils/api/apiClient");`,
+    `const database=require('database')`,
+    `const network = require(     'network'     )`,
+    `var moment = require(    "moment")`,
+    `    var faker = require("faker"       )   `,
   ];
   test('importSample1 example', async () => {
     const actual = coreUtils.parseLegacyImportLines(fileNameSample1, importSample1);
 
-    expect(actual).toMatchInlineSnapshot()
-
-    // expect(actual.libraryImportMap).toMatchSnapshot();
-    // expect(actual.moduleUsageMap).toMatchSnapshot();
-    // expect(actual.importedModules).toMatchSnapshot();
+    expect(actual.libraryImportMap).toMatchSnapshot();
+    expect(actual.moduleUsageMap).toMatchSnapshot();
+    expect(actual.importedModules).toMatchSnapshot();
   });
 });
