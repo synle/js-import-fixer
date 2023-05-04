12003e65f585edd841df3201b7abe7384dcd0681
Joël Charles <joel.charles91@gmail.com>
2023-05-04 16:12:59 +0200

    feat: forward dontWriteToOutputFile

--- src/library.ts
+++ src/library.ts
@@ -4,9 +4,10 @@ const fixImport = (
   file: string,
   content: string,
   externalPackages: string[] = [],
+  dontWriteToOutputFile = false,
   libUsageStats = {},
 ) => {
-  return coreUtils.process(file, content, externalPackages, false, (libUsageStats = {}));
+  return coreUtils.process(file, content, externalPackages, dontWriteToOutputFile, (libUsageStats = {}));
 };
 
 module.exports = {
